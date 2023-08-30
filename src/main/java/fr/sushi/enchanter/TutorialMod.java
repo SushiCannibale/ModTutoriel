ed352e22a5eebd0b86de7cabae5f7bae9f7f7aff
Marc-Aurèle <marc-aurele.lepine@epita.fr>
2023-08-30 13:13:13 +0200

    base

---
+++ src/main/java/fr/sushi/enchanter/TutorialMod.java
@@ -0,0 +1,14 @@
+package fr.sushi.enchanter;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TutorialMod.MODID)
+public class TutorialMod
+{
+    public static final String MODID = "tutorialmod";
+
+    public TutorialMod()
+    {
+
+    }
+}
